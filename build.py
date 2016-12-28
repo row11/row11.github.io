6fcb19265f920c3c00cea1d83173b5c3e77eb7e8
Roie Levin <roiel@allenai.org>
2016-12-28 03:00:32 -0800

    adding build tool

---
+++ build.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+import os, dateutil.parser
+import xml.etree.ElementTree as ET
+entries = os.listdir("entries")
+front_page = ""
+template = open("template", "r").read()
+post_template = content = open("post_template", "r").read()
+max_date = dateutil.parser.parse("September 1, 1901")
+
+# Collect posts
+posts = []
+for entry in entries:
+    path_prefix = "entries/{0}/".format(entry)
+    tree = ET.parse(path_prefix + "metadata.xml").getroot().find("post")
+    title = tree.get("title")
+    date = tree.get("date")
+    link = path_prefix + "index.html"
+    result = post_template.replace("!!!title!!!", title).replace("!!!date!!!", date).replace("!!!link!!!", link)
+    posts.append((result,dateutil.parser.parse(date)))
+
+posts_string = "\n".join(map(lambda post: post[0], sorted(posts, key=lambda post: post[1])))
+
+for entry in entries:
+    path_prefix = "entries/{0}/".format(entry)
+    tree = ET.parse(path_prefix + "metadata.xml").getroot().find("post")
+    title = tree.get("title")
+    date = tree.get("date")
+    img = tree.get("img")
+    content = open(path_prefix + "content.txt", "r").read()
+    # Set the most recent post as the homepage
+    formal_date = dateutil.parser.parse(date)
+    result = template.replace("!!!title!!!", title).replace("!!!date!!!", date).replace("!!!posts!!!", posts_string).replace("!!!content!!!", content).replace("!!!image!!!", img)
+    open(path_prefix + "index.html", "w").write(result)
+    if formal_date > max_date:
+        max_date = formal_date
+        front_page = result
+
+open("index.html", "w").write(front_page.replace("../../",""))
